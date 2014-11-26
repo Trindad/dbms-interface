747a5f0e1412672648401a77e7761e52862a4735
RogerioSchmidt <rogelho.schmidt@gmail.com>
2014-11-26 16:29:55 -0200

    Criado exemplo de main com as chamadas de funções

---
+++ exemploMain.c
@@ -0,0 +1,21 @@
+/*/////////////////////////////////////////////////////////////////////////////////*/
+//  Alunos: Natan J. Mai, Ricardo Zanuzzo e Rogério Torchelsen                    //
+//  Disciplina: Banco de Dados II                                                 //      
+//  Curso: Ciência da Computação                                                  //      
+//  Universidade Federal da Fronteira Sul                                         //      
+//                                                                                //
+//////////////////////////////////////////////////////////////////////////////////*/
+
+#include <stdio.h>
+
+
+/***********************************************************************************|
+|* EXEMPLO DE MAIN QUE CHAMARÁ AS FUNÇÕEs:
+criaTabela(char nomeT, char atrib1, int tipo, char
+
+      */   
+
+int main(){
+    menu();
+    return 0;
+}
