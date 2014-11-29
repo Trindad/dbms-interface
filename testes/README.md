fbec613731cc77cde049c871f9e474500b6770ee
RogerioSchmidt <rogelho.schmidt@gmail.com>
2014-11-29 12:45:20 -0200

    Separei em outra pasta pra mexer e depois volto lá

---
+++ testes/README.md
@@ -0,0 +1,10 @@
+<b>Trabalho de Banco de Dados II</b>
+
+<i>Alunos:</i> Rogério Torchelsen, Ricardo Zanuzzo e Natan J. Mai.
+
+<i>Descrição:</i>
+Criar um programa que utilize as bibliotecas. 
+O programa deverá criar um banco de dados (pelo menos quatro tabelas), 
+inserir dados no banco criado e recuperar as tuplas das tabelas.
+
+
--- testes/exemploMain.c
+++ testes/exemploMain.c
@@ -7,8 +7,10 @@
 //////////////////////////////////////////////////////////////////////////////////*/
 
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <string.h>
+#include "buffend.h"
+#include "funcoesNRR.h"
 /***********************************************************************************|
 |* EXEMPLO DE MAIN QUE CHAMARÁ AS FUNÇÕEs:
 criaTabela(char nomeTabela);
@@ -20,11 +22,14 @@ int main(){
     nrTabelas = 4;
     table  *tab[nrTabelas];	
     
+    //aqui cria as variáveis que receberão os retornos das chamadas de funções
+    int tabela1, tabela2;
     object         = existeArquivo("fs_object.dat");
     schema         = existeArquivo("fs_schema.dat");
     tabela1        = existeArquivo("tabela1.dat");
     tabela2        = existeArquivo("tabela2.dat");
-    if(!tabela1){
+    
+    if(!tabela1){ //se ainda não existe a tabela, a mesma é criada
             tab[0] = iniciaTabela("tabela1");   
             tab[0] = adicionaCampo(tab[0], "atrib1" , 'S', 20);
             tab[0] = adicionaCampo(tab[0], "atrib2" , 'I', (sizeof(int)));
@@ -33,16 +38,14 @@ int main(){
             tab[0] = adicionaCampo(tab[0], "atrib5" , 'I', (sizeof(int)));
             tab[0] = adicionaCampo(tab[0], "atrib6" , 'S', 10);
             erro   = finalizaTabela(tab[0]);
-        }
+     }
 
-        //Modelo
-        if(!tabela2){
+        if(!tabela2){	////se ainda não existe a tabela, a mesma é criada
             tab[1] = iniciaTabela("Tabela2"); 
             tab[1] = adicionaCampo(tab[1], "atrib1" , 'I', (sizeof(int)));
             tab[1] = adicionaCampo(tab[1], "atrib2" , 'S', 20);
             tab[1] = adicionaCampo(tab[1], "atrib3" , 'I', (sizeof(int)));
             erro   = finalizaTabela(tab[1]);
         }
-   
     return 0;
 }
