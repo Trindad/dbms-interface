822222540bd23bbbbcf548733b76ade0706f011b
zanuzzorz <zanuzzorz@gmail.com>
2014-12-14 15:01:52 -0200

    Atulização main de teste

--- PK_FK/mainTeste.c
+++ PK_FK/mainTeste.c
@@ -25,14 +25,62 @@ int main(){
 			tab[0] = adicionaCampo(tab[0], "atrib14" , 'D', (sizeof(double)),NPK,"","");
 			finalizaTabela(tab[0]);
 		 }
-	 }
+		 if(!nTabela[1]){	//Se ainda não existe a tabela2, a mesma é criada
+			tab[1] = iniciaTabela("tabela2"); 
+			tab[1] = adicionaCampo(tab[1], "atrib21" , 'I', (sizeof(int))	,PK, "","");
+			tab[1] = adicionaCampo(tab[1], "atrib22" , 'S', 20				,NPK, "","");
+			tab[1] = adicionaCampo(tab[1], "atrib23" , 'I', (sizeof(int))	,NPK, "","");
+			tab[1] = adicionaCampo(tab[1], "atrib24" , 'S', 10				,NPK, "","");
+			finalizaTabela(tab[1]);
+		}
+		if(!nTabela[2]){	//Se ainda não existe a tabela3, a mesma é criada
+			tab[2] = iniciaTabela("tabela3"); 
+			tab[2] = adicionaCampo(tab[2], "atrib11" , 'I', (sizeof(int))	,FK, "tabela1","atrib11");
+			tab[2] = adicionaCampo(tab[2], "atrib32" , 'C', 20				,NPK, "","");
+			finalizaTabela(tab[2]);
+		}
+	}
 	 
-	 	//Inserção de tuplas na tabela1        
+	 //Inserção de tuplas na tabela1        
 	colunas = insereValor(colunas, "atrib11", "10");
 	colunas = insereValor(colunas, "atrib12", "12");
 	colunas = insereValor(colunas, "atrib13", "teste13");
 	colunas = insereValor(colunas, "atrib14", "1.4");
 	erro = finalizaInsert("tabela1", colunas); 
+	
+	colunas = insereValor(colunas, "atrib11", "11");
+	colunas = insereValor(colunas, "atrib12", "30");
+	colunas = insereValor(colunas, "atrib13", "teste34"	);
+	colunas = insereValor(colunas, "atrib14", "1.7");
+	finalizaInsert("tabela1", colunas); 
+
+
+	colunas = insereValor(colunas, "atrib11", "12");
+	colunas = insereValor(colunas, "atrib12", "33");
+	colunas = insereValor(colunas, "atrib13", "teste340");
+	colunas = insereValor(colunas, "atrib14", "1.9");
+	finalizaInsert("tabela1", colunas); 
+	
+	//Inserção de tuplas na tabela2	
+	colunas = insereValor(colunas, "atrib21", "10");
+	colunas = insereValor(colunas, "atrib22", "dois");
+	colunas = insereValor(colunas, "atrib23", "23");
+	colunas = insereValor(colunas, "atrib24", "tres");
+	finalizaInsert("tabela2", colunas);
+	
+	colunas = insereValor(colunas, "atrib21", "11");
+	colunas = insereValor(colunas, "atrib22", "dois");
+	colunas = insereValor(colunas, "atrib23", "23");
+	colunas = insereValor(colunas, "atrib24", "tres");
+	
+	finalizaInsert("tabela2", colunas);
+	
+	
+	//Inserção de tupla na tabela3
+	colunas = insereValor(colunas, "atrib11", "10");
+	colunas = insereValor(colunas, "atrib32", "M");
+	
+	finalizaInsert("tabela3", colunas);
 	
 	if(erro == ERRO_CHAVE_PRIMARIA) {
 		printf("ERRO DE CHAVE PRIMARIA !!! \n");
@@ -45,6 +93,8 @@ int main(){
 		
 	
 	imprime("tabela1");
+	imprime("tabela2");
+	imprime("tabela3");
 
     
     return 0;
