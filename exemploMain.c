b261773398b3b08648ea17f9e9d181b66f90f6d2
Silvana Trindade <syletri@gmail.com>
2015-05-24 21:07:48 -0300

    Inserindo nas tabelas somente na primeira vez e comentando exclusão de tabelas

--- exemploMain.c
+++ exemploMain.c
@@ -41,83 +41,83 @@ int main(){
             finalizaTabela(tab[2]);
         }
        
+         
+         //Inserção de tuplas na tabela1   
+        colunas = NULL;     
+        colunas = insereValor(tab[0],colunas, "CPF", "123456");
+        colunas = insereValor(tab[0],colunas, "Nome", "Rogerio");
+        colunas = insereValor(tab[0],colunas, "Endereco", "Rua Marechal");
+        colunas = insereValor(tab[0],colunas, "Peso", "81.4");
+        finalizaInsert("Aluno", colunas); 
+        
+        colunas = NULL;     
+        colunas = insereValor(tab[0],colunas, "CPF", "654321");
+        colunas = insereValor(tab[0],colunas, "Nome", "Ricardo");
+        colunas = insereValor(tab[0],colunas, "Endereco", "RuaClevela");
+        colunas = insereValor(tab[0],colunas, "Peso", "88.9");
+        finalizaInsert("Aluno", colunas); 
+
+        colunas = NULL;     
+        colunas = insereValor(tab[0],colunas, "CPF", "1234567");
+        colunas = insereValor(tab[0],colunas, "Nome", "Natan");
+        colunas = insereValor(tab[0],colunas, "Endereco", "RuaDelmi");
+        colunas = insereValor(tab[0],colunas, "Peso", "58.9");
+        finalizaInsert("Aluno", colunas); 
+            
+        
+        //Inserção de tuplas na tabela2 
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "111");
+        colunas = insereValor(tab[1],colunas, "Nome", "UFFS");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadeTerra");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaChuva");
+        finalizaInsert("Inst", colunas);
+        
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "222");
+        colunas = insereValor(tab[1],colunas, "Nome", "CEFET");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadePedra");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaVento");
+        finalizaInsert("Inst", colunas);
+
+        colunas = NULL;
+        colunas = insereValor(tab[1],colunas, "CodInst", "333");
+        colunas = insereValor(tab[1],colunas, "Nome", "UNOESC");
+        colunas = insereValor(tab[1],colunas, "Endereco", "RuadeAsfal");
+        colunas = insereValor(tab[1],colunas, "Reitor", "MandaAgua");
+        finalizaInsert("Inst", colunas);
+        
+        
+        
+        //Inserção de tupla na tabela3
+        colunas = NULL;
+        colunas = insereValor(tab[2],colunas, "CodMat", "1401");
+        colunas = insereValor(tab[2],colunas, "CPF", "123456");
+        colunas = insereValor(tab[2],colunas, "CodInst", "333");
+        colunas = insereValor(tab[2],colunas, "Curso", "CC");
+        finalizaInsert("Inscri", colunas);
+        
+        colunas = NULL;
+        colunas = insereValor(tab[2],colunas, "CodMat", "1402");
+        colunas = insereValor(tab[2],colunas, "CPF", "654321");
+        colunas = insereValor(tab[2],colunas, "CodInst", "222");
+        colunas = insereValor(tab[2],colunas, "Curso", "CC");
+        finalizaInsert("Inscri", colunas);
+        
+        colunas = NULL;
+        colunas = insereValor(tab[2],colunas, "CodMat", "1403");
+        colunas = insereValor(tab[2],colunas, "CPF", "1234567");
+        colunas = insereValor(tab[2],colunas, "CodInst", "111");
+        colunas = insereValor(tab[2],colunas, "Curso", "ADM");
+        finalizaInsert("Inscri", colunas);
     }
     
-     
-     //Inserção de tuplas na tabela1   
-    colunas = NULL;     
-    colunas = insereValor(tab[0],colunas, "CPF", "123456");
-    colunas = insereValor(tab[0],colunas, "Nome", "Rogerio");
-    colunas = insereValor(tab[0],colunas, "Endereco", "Rua Marechal");
-    colunas = insereValor(tab[0],colunas, "Peso", "81.4");
-    finalizaInsert("Aluno", colunas); 
-    
-    colunas = NULL;     
-    colunas = insereValor(tab[0],colunas, "CPF", "654321");
-    colunas = insereValor(tab[0],colunas, "Nome", "Ricardo");
-    colunas = insereValor(tab[0],colunas, "Endereco", "RuaClevela");
-    colunas = insereValor(tab[0],colunas, "Peso", "88.9");
-    finalizaInsert("Aluno", colunas); 
-
-    colunas = NULL;     
-    colunas = insereValor(tab[0],colunas, "CPF", "1234567");
-    colunas = insereValor(tab[0],colunas, "Nome", "Natan");
-    colunas = insereValor(tab[0],colunas, "Endereco", "RuaDelmi");
-    colunas = insereValor(tab[0],colunas, "Peso", "58.9");
-    finalizaInsert("Aluno", colunas); 
-        
-    
-    //Inserção de tuplas na tabela2 
-    colunas = NULL;
-    colunas = insereValor(tab[1],colunas, "CodInst", "111");
-    colunas = insereValor(tab[1],colunas, "Nome", "UFFS");
-    colunas = insereValor(tab[1],colunas, "Endereco", "RuadeTerra");
-    colunas = insereValor(tab[1],colunas, "Reitor", "MandaChuva");
-    finalizaInsert("Inst", colunas);
-    
-    colunas = NULL;
-    colunas = insereValor(tab[1],colunas, "CodInst", "222");
-    colunas = insereValor(tab[1],colunas, "Nome", "CEFET");
-    colunas = insereValor(tab[1],colunas, "Endereco", "RuadePedra");
-    colunas = insereValor(tab[1],colunas, "Reitor", "MandaVento");
-    finalizaInsert("Inst", colunas);
-
-    colunas = NULL;
-    colunas = insereValor(tab[1],colunas, "CodInst", "333");
-    colunas = insereValor(tab[1],colunas, "Nome", "UNOESC");
-    colunas = insereValor(tab[1],colunas, "Endereco", "RuadeAsfal");
-    colunas = insereValor(tab[1],colunas, "Reitor", "MandaAgua");
-    finalizaInsert("Inst", colunas);
-    
-    
-    
-    //Inserção de tupla na tabela3
-    colunas = NULL;
-    colunas = insereValor(tab[2],colunas, "CodMat", "1401");
-    colunas = insereValor(tab[2],colunas, "CPF", "123456");
-    colunas = insereValor(tab[2],colunas, "CodInst", "333");
-    colunas = insereValor(tab[2],colunas, "Curso", "CC");
-    finalizaInsert("Inscri", colunas);
-    
-    colunas = NULL;
-    colunas = insereValor(tab[2],colunas, "CodMat", "1402");
-    colunas = insereValor(tab[2],colunas, "CPF", "654321");
-    colunas = insereValor(tab[2],colunas, "CodInst", "222");
-    colunas = insereValor(tab[2],colunas, "Curso", "CC");
-    finalizaInsert("Inscri", colunas);
-    
-    colunas = NULL;
-    colunas = insereValor(tab[2],colunas, "CodMat", "1403");
-    colunas = insereValor(tab[2],colunas, "CPF", "1234567");
-    colunas = insereValor(tab[2],colunas, "CodInst", "111");
-    colunas = insereValor(tab[2],colunas, "Curso", "ADM");
-    finalizaInsert("Inscri", colunas);
     
     imprime("Aluno");        //Imprime os atributos da tabela "Aluno"
     imprime("Inst");
-    excluirTabela("Inst");   //Exclui os dados da tabela do dicionario e remove-a do disco
+    //excluirTabela("Inst");   //Exclui os dados da tabela do dicionario e remove-a do disco
     imprime("Inscri");
-    excluirTabela("Inscri");
+    //excluirTabela("Inscri");
     
     
     return 0;
